ecb79da7a9b0de8ef094f6885aca8f79c876d4ef
softfiredev <braikbraik06@gmail.com>
2024-10-18 17:07:15 +0200

    version 18/10/2024 17:07

---
+++ data_acquisition.py
@@ -0,0 +1,9 @@
+import os
+import nibabel as nib
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Specify the path to the NIfTI file
+nii_file_path = ['C:\\Users\\R I B\\Desktop\\Medical Segmentation\\Medical-Image-Segmentation\\DCM03-OH-AL_V2_12.nii']  # Update this path 
+
+
